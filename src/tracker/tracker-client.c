491d6843474ba31ffc89c19de95215c516810b73
Greg Bruno <greg.bruno@gmail.com>
2010-08-19 17:45:21 +0000

    first attempt at adding MD5 verification

--- src/tracker/tracker-client.c
+++ src/tracker/tracker-client.c
@@ -1,10 +1,13 @@
 /*
- * $Id: tracker-client.c,v 1.14 2010/04/15 15:30:54 bruno Exp $
+ * $Id: tracker-client.c,v 1.15 2010/08/19 17:45:21 bruno Exp $
  *
  * @COPYRIGHT@
  * @COPYRIGHT@
  *
  * $Log: tracker-client.c,v $
+ * Revision 1.15  2010/08/19 17:45:21  bruno
+ * first attempt at adding MD5 verification
+ *
  * Revision 1.14  2010/04/15 15:30:54  bruno
  * better coop processing. the simulation is now performing 20% better.
  *
@@ -90,6 +93,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <libgen.h>
+#include <openssl/md5.h>
 
 extern int init(uint16_t *, char *, in_addr_t *, uint16_t *, char *, uint16_t *,
 	in_addr_t *);
@@ -99,6 +103,8 @@ extern void logmsg(const char *, ...);
 extern int send_msg(int, in_addr_t *, uint16_t);
 
 int	status = HTTP_OK;
+MD5_CTX	context;
+
 
 int
 getargs(char *forminfo, char *filename)
@@ -149,6 +155,10 @@ dobody(void *ptr, size_t size, size_t nmemb, void *stream)
 {
 	if ((status >= HTTP_OK) && (status <= HTTP_MULTI_STATUS)) {
 		fwrite(ptr, size, nmemb, stream);
+
+		if (MD5_Update(&context, ptr, size * nmemb) != 1) {
+			logmsg("dobody:MD5_Update failed\n");
+		}
 	}
 
 	return(size * nmemb);
@@ -337,6 +347,88 @@ outputfile(char *filename, char *range)
 	return(0);
 }
 
+int
+verify_md5(char *filename, char *md5)
+{
+	int		i;
+	unsigned char	digest[16];
+	char		digest_str[64];
+
+	/*
+	 * get the digest for the file we just read, then make it a string
+	 */
+	if (MD5_Final(digest, &context) != 1) {
+		logmsg("verify_md5:MD5_Final failed\n");
+		return(0);
+	}
+
+	bzero(digest_str, sizeof(digest_str));
+
+	for (i = 0; i < sizeof(digest) ; ++i) {
+		sprintf(digest_str, "%s%02x", digest_str,
+			(unsigned char)digest[i]);
+	}
+
+	if (strcmp(digest_str, md5) == 0) {
+		return(1);
+	}
+
+	return(-1);
+}
+
+/*
+ * check the MD5 for a file.
+ *
+ * return values:
+ *
+ *	 0 = no packages.md5 file or no entry in packages.md5 for the filename
+ *	 1 = checksum passed
+ *	-1 = checksum failed
+ */
+int
+check_md5(char *filename)
+{
+	FILE	*file;
+	int	retval;
+	int	passed;
+	char	digest[64];
+	char	fname[1024];
+	char	done;
+
+	passed = 0;
+
+	if ((file = fopen("/tmp/product/packages.md5", "r")) == NULL) {
+		logmsg("check_md5:fopen:failed\n");
+		return(passed);
+	}
+
+	done = 0;
+	while (!done) {
+		retval = fscanf(file, "%s %s", digest, fname);
+
+		if (retval == 2) {
+			if (strcmp(fname, filename) == 0) {
+				if (verify_md5(filename, digest) == 1) {
+					logmsg("MD5 checksum passed for file %s\n", filename);
+					passed = 1;
+				} else {
+					logmsg("MD5 checksum failed for file %s\n", filename);
+					passed = -1;
+				}
+
+				done = 1;
+				continue;
+			}
+		} else if (retval == EOF) {
+			done = 1;
+			continue;
+		}
+	}
+
+	return(passed);
+}
+
+
 unsigned long long	curltime;
 
 int
@@ -372,6 +464,11 @@ downloadfile(CURL *curlhandle, char *url, char *range)
 	gettimeofday(&start_time, NULL);
 #endif
 
+	if (MD5_Init(&context) != 1) {
+		fprintf(stderr, "MD5_Init failed\n");
+		exit(-1);
+	}
+
 	if ((curlcode = curl_easy_perform(curlhandle)) != CURLE_OK) {
 		logmsg("downloadfile:curl_easy_perform():failed:(%d)\n",
 			curlcode);
@@ -385,6 +482,26 @@ downloadfile(CURL *curlhandle, char *url, char *range)
 	logmsg("downloadfile:svc time: %lld usec:url %s\n", (e - s), url);
 	curltime = e - s;
 #endif
+
+	/*
+	 * if this is not a range request, then do an MD5 checksum of the file
+	 */
+	if (range == NULL) {
+		char	*ptr = rindex(url, '/');
+		char	*filename;
+
+		if (ptr) {
+			if (*ptr == '/') {
+				filename = ptr + 1;
+			} else {
+				filename = ptr;
+			}
+
+			if (check_md5(filename) != 1) {
+				return(-1);
+			}
+		}
+	}
 
 	return(0);
 }
