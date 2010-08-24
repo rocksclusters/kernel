01f8a37227f3a54f450bbdb8f0346860b050ee62
Greg Bruno <greg.bruno@gmail.com>
2010-08-24 20:43:04 +0000

    md5 checksum tweaks

--- src/tracker/tracker-client.c
+++ src/tracker/tracker-client.c
@@ -1,10 +1,13 @@
 /*
- * $Id: tracker-client.c,v 1.15 2010/08/19 17:45:21 bruno Exp $
+ * $Id: tracker-client.c,v 1.16 2010/08/24 20:43:04 bruno Exp $
  *
  * @COPYRIGHT@
  * @COPYRIGHT@
  *
  * $Log: tracker-client.c,v $
+ * Revision 1.16  2010/08/24 20:43:04  bruno
+ * md5 checksum tweaks
+ *
  * Revision 1.15  2010/08/19 17:45:21  bruno
  * first attempt at adding MD5 verification
  *
@@ -101,6 +104,7 @@ extern int lookup(int, in_addr_t *, uint64_t, tracker_info_t **);
 extern int register_hash(int, in_addr_t *, uint32_t, tracker_info_t *);
 extern void logmsg(const char *, ...);
 extern int send_msg(int, in_addr_t *, uint16_t);
+extern int check_md5(char *);
 
 int	status = HTTP_OK;
 MD5_CTX	context;
@@ -347,94 +351,13 @@ outputfile(char *filename, char *range)
 	return(0);
 }
 
-int
-verify_md5(char *filename, char *md5)
-{
-	int		i;
-	unsigned char	digest[16];
-	char		digest_str[64];
-
-	/*
-	 * get the digest for the file we just read, then make it a string
-	 */
-	if (MD5_Final(digest, &context) != 1) {
-		logmsg("verify_md5:MD5_Final failed\n");
-		return(0);
-	}
-
-	bzero(digest_str, sizeof(digest_str));
-
-	for (i = 0; i < sizeof(digest) ; ++i) {
-		sprintf(digest_str, "%s%02x", digest_str,
-			(unsigned char)digest[i]);
-	}
-
-	if (strcmp(digest_str, md5) == 0) {
-		return(1);
-	}
-
-	return(-1);
-}
-
-/*
- * check the MD5 for a file.
- *
- * return values:
- *
- *	 0 = no packages.md5 file or no entry in packages.md5 for the filename
- *	 1 = checksum passed
- *	-1 = checksum failed
- */
-int
-check_md5(char *filename)
-{
-	FILE	*file;
-	int	retval;
-	int	passed;
-	char	digest[64];
-	char	fname[1024];
-	char	done;
-
-	passed = 0;
-
-	if ((file = fopen("/tmp/product/packages.md5", "r")) == NULL) {
-		logmsg("check_md5:fopen:failed\n");
-		return(passed);
-	}
-
-	done = 0;
-	while (!done) {
-		retval = fscanf(file, "%s %s", digest, fname);
-
-		if (retval == 2) {
-			if (strcmp(fname, filename) == 0) {
-				if (verify_md5(filename, digest) == 1) {
-					logmsg("MD5 checksum passed for file %s\n", filename);
-					passed = 1;
-				} else {
-					logmsg("MD5 checksum failed for file %s\n", filename);
-					passed = -1;
-				}
-
-				done = 1;
-				continue;
-			}
-		} else if (retval == EOF) {
-			done = 1;
-			continue;
-		}
-	}
-
-	return(passed);
-}
-
-
 unsigned long long	curltime;
 
 int
 downloadfile(CURL *curlhandle, char *url, char *range)
 {
 	CURLcode		curlcode;
+	int			retval;
 #ifdef	TIMEIT
 	struct timeval		start_time, end_time;
 	unsigned long long	s, e;
@@ -497,7 +420,19 @@ downloadfile(CURL *curlhandle, char *url, char *range)
 				filename = ptr;
 			}
 
-			if (check_md5(filename) != 1) {
+			retval = check_md5(filename);
+
+			/*
+			 * check_md5 returns:
+			 *
+			 *	0 - when the filename is not found in the
+			 *	packages.md5 file
+			 *
+			 *	1 - checksum passed
+			 *
+			 *	-1 - checksum failed
+			 */
+			if (retval == -1) {
 				return(-1);
 			}
 		}
