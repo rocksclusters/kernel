4cebc2415a947a5f547421c7fae37f5a60c95330
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-03-02 07:10:33 +0000

    Change name to foundation-sqlite to avoid conflict with system sqlite

--- src/sqlite/version.mk
+++ src/sqlite/version.mk
@@ -1,8 +1,8 @@
-NAME		= sqlite
+NAME		= foundation-sqlite
 PKGROOT		= /opt/sqlite
 VERSION		= 3.7.10
 TARVERSION	= 3071000
 RELEASE		= 0
-SUBDIR		= $(NAME)-autoconf-$(TARVERSION)
+SUBDIR		= sqlite-autoconf-$(TARVERSION)
 TARFILE		= $(SUBDIR).tar.gz
 
