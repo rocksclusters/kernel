b3e08fea16d67c4096916d713832dab76594bddb
Luca Clementi <luca.clementi@gmail.com>
2012-05-17 23:22:41 +0000

    committed by mistake not supposed to be checked in

--- src/rocks-boot/enterprise/6/loader/patch-files/anaconda-13.21.149/loader/loader.c
+++ src/rocks-boot/enterprise/6/loader/patch-files/anaconda-13.21.149/loader/loader.c
@@ -2715,10 +2715,6 @@ int main(int argc, char ** argv) {
         fprintf(stderr, "Anaconda died after receiving signal %d.\n", ret);
     }
 
-    //let's try to eject before rebooting
-    execl("/usr/bin/eject", "/usr/bin/eject", NULL);
-
-
     if ((rc == 0) && (FL_POWEROFF(flags) || FL_HALT(flags))) {
         if (!(pid = fork())) {
             char * cmd = (FL_POWEROFF(flags) ? strdup("/sbin/poweroff") :
