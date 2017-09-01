e097d27a7d3be52026fc463d4d2e2a618366c4ed
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-31 19:35:12 -0700

    modify isolinux.cfg to have have central= on append lines

--- src/rocks-boot-7/version.mk
+++ src/rocks-boot-7/version.mk
@@ -3,3 +3,11 @@ PKGROOT		= /boot/kickstart/default
 RELEASE		= 0
 RPM.PREFIX	= $(PKGROOT)
 INSTALLERISO	= rocks-installer-$(VERSION.MAJOR)-$(VERSION.MINOR).iso
+
+# ISFINAL = --isfinal
+ifdef ISFINAL
+CENTRAL=central-$(VERSION)-x86-64.rocksclusters.org
+else
+CENTRAL=beta7.rocksclusters.org
+endif
+RPM.DESCRIPTION = rocks-boot installer media with central=$(CENTRAL)
